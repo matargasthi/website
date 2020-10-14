3734a65801e7cf10585204e2e593d7ebe4d01986
patelaryan7751 <59426397+patelaryan7751@users.noreply.github.com>
2020-10-14 07:33:11 +0530

    update in ui

---
+++ app.js
@@ -0,0 +1,14 @@
+var defferedPrompt;
+if ('serviceWorker' in navigator){
+    navigator.serviceWorker.register('sw.js').then(function(){
+console.log("sw registered");
+    });
+}
+window.addEventListener('beforeinstallprompt',function(event){
+console.log('beforeinstallprompt fired');
+event.preventDefault();
+defferedPrompt=event;
+     document.querySelector('#installapp').style.display = 'block';
+    
+return false;
+});
